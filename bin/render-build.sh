f7524704661335d71a95a7472fbda284f757684c
ibtechraj <krs30018@gmail.com>
2024-07-26 18:19:40 +0530

    trying react-rails deployment

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -7,5 +7,5 @@ bundle exec rails webpacker:install
 gem install foreman
 # bin/webpack-dev-server
 
-# bundle exec rails assets:precompile
-# bundle exec rails assets:clean
+bundle exec rails assets:precompile
+bundle exec rails assets:clean
