265a0cb1263c741fd38bc50314709867428a46e8
ibtechraj <krs30018@gmail.com>
2024-07-30 17:18:38 +0530

    debugging deployment errors

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -6,6 +6,6 @@ bundle install
 bundle exec rails webpacker:install
 gem install foreman
 # bin/webpack-dev-server
-bundle exec rails assets:clean RAILS_ENV=production.
+bundle exec rails assets:clean RAILS_ENV=production
 
-bundle exec rails assets:precompile RAILS_ENV=production.
+# bundle exec rails assets:precompile RAILS_ENV=production.
